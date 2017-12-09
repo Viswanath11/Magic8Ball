82897dc6a683f794312425f4b5967e8c7c00f182
Viswanath Subramani S S <viswanathsubramaniss@gmail.com>
2017-12-09 11:34:10 +0530

    Complete Commit

---
+++ Magic8Ball/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Magic8Ball
+//
+//  Created by Viswanath Subramani S S on 28/10/17.
+//  Copyright © 2017 ViswanathSubramaniSS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+	
+	@IBOutlet weak var BallImageView: UIImageView!
+	
+	@IBAction func ShakeButtonPressed(_ sender: UIButton) {
+		updateBallImage()
+	}
+	
+	var randomIndex = 0
+	let ballImage = ["ball1","ball2","ball3","ball4","ball5"]
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+	func updateBallImage(){
+		randomIndex = Int(arc4random_uniform(5))
+		BallImageView.image = UIImage(named: ballImage[randomIndex])
+	}
+	
+	override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+		updateBallImage()
+	}
+
+}
+
